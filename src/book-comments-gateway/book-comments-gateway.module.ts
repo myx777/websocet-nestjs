21ad5b67579903b281b270f993b8bb497bc2c1bf
myx777 <anton.kruglov89@gmail.com>
2024-11-08 20:34:28 +0300

    correct

--- src/book-comments-gateway/book-comments-gateway.module.ts
+++ src/book-comments-gateway/book-comments-gateway.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BookCommentsGateway } from './book-comments/book-comments.gateway';
+import { BookCommentsModule } from 'src/book-comments/book-comments.module';
 
 @Module({
+  imports: [BookCommentsModule],
   providers: [BookCommentsGateway]
 })
 export class BookCommentsGatewayModule {}
